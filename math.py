5824210a98e9c428f3f194b7a03cb3e2373d1083
gracekazungu <kazungungrace@gmail.com>
2023-03-31 19:48:31 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(c,d):
+    answer2= c-d
+    return answer2
+
+def divide(e,f):
+    answer3=e/f   
+    return answer3
+
+def multiply(g,h):
+    answer4=g*h
+    return answer4
+
+def remainder(i,j):
+    answer5=i%j
+    return answer5
